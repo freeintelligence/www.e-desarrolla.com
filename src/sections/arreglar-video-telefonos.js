7d46d713fc540e0633bace1f9e5cc33726051fe5
Luis Arriaza <free04381@gmail.com>
2024-04-19 13:03:57 -0400

    Prueba..

--- src/sections/arreglar-video-telefonos.js
+++ src/sections/arreglar-video-telefonos.js
@@ -5,13 +5,13 @@ module.exports = function () {
     return
   }
 
-  var urlVideo =
-    'https://www.youtube.com/embed/kd99twk-iWM?autohide=1&autoplay=1&mute=1&controls=0&enablejsapi=1&iv_load_policy=3&loop=1&modestbranding=1&playsinline=1&rel=0&showinfo=0&wmode=opaque&origin=https%3A%2F%2Fwww.e-desarrolla.com&widgetid=1'
-  var cssSelector = '.sqs-video-background iframe'
+  var cssSelector = '.sqs-video-background'
   var element = document.querySelector(cssSelector)
 
+  var iFrame = `<iframe id="player" style="position: absolute; inset: 0px 0px 0px -234.222px; width: 1324.44px; height: 745px;" frameborder="0" allowfullscreen="" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" title="e desarrolla" width="640" height="360" src="https://www.youtube.com/embed/kd99twk-iWM?autohide=1&amp;autoplay=1&mute=1&amp;controls=0&amp;enablejsapi=1&amp;iv_load_policy=3&amp;loop=1&amp;modestbranding=1&amp;playsinline=1&amp;rel=0&amp;showinfo=0&amp;wmode=opaque&amp;origin=https%3A%2F%2Fwww.e-desarrolla.com&amp;widgetid=1" class="background-video ready"></iframe>`
+
   if (element) {
-    element.src = urlVideo
+    element.innerHTML = iFrame
   }
 
   alreadyExecuted = true
